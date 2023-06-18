93e5f5fb3af5c9d6295b28ad0e6dcca7e83f5f32
Ha Yen <hanguyentieuyen@gmail.com>
2023-06-19 00:46:05 +0700

    header layout

---
+++ src/useRouteElement.tsx
@@ -0,0 +1,30 @@
+import { useRoutes } from 'react-router-dom'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import RegisterLayout from './layouts/RegisterLayout'
+export default function useRouteElement() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElements
+}
