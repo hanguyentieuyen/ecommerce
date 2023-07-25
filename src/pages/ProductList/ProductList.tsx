55078d205d2cd877a3af2a2b6f1bfefbbce07451
Ha Yen <hanguyentieuyen@gmail.com>
2023-07-26 00:21:40 +0700

    aside layout

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -5,7 +5,7 @@ export default function ProductList() {
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
-        <div className='gid-cols-12 grid gap-6'>
+        <div className='grid grid-cols-12 gap-6'>
           <div className='col-span-3'>
             <AsideFilter />
           </div>
