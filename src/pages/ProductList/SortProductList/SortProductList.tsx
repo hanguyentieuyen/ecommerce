c7bc9e42b666849d2ababda0e28ee04aaea1dd73
Ha Yen <hanguyentieuyen@gmail.com>
2023-07-30 01:08:08 +0700

    update sort list ui

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -13,7 +13,7 @@ export default function SortProductList() {
           <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
             Bán chạy
           </button>
-          <select className='h-8 bg-white px-4 text-left text-sm capitalize text-black hover:bg-slate-100'>
+          <select className='h-8 bg-white px-4 text-left text-sm capitalize text-black outline-none hover:bg-slate-100'>
             <option value='' disabled>
               Giá
             </option>
@@ -21,29 +21,37 @@ export default function SortProductList() {
             <option value='price:desc'>Giá: Cao đến thấp</option>
           </select>
         </div>
-      </div>
-      <div className='flex items-center'>
-        <div>
-          <span className='text-orange'>1</span>
-          <span>2</span>
-        </div>
-        <div className='ml-2'>
-          <button className='h-8 cursor-not-allowed rounded-tl-sm bg-white/60 px-3 hover:bg-slate-100'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='h-3 w-3'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5'
-              />
-            </svg>
-          </button>
+        <div className='flex items-center'>
+          <div>
+            <span className='text-orange'>1</span>
+            <span>2</span>
+          </div>
+          <div className='ml-2'>
+            <button className='h-8 cursor-not-allowed rounded-bl-sm rounded-tl-sm bg-white/60 px-3 shadow hover:bg-slate-100'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='h-3 w-3'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+              </svg>
+            </button>
+            <button className='h-8 rounded-br-sm rounded-tr-sm bg-white px-3 shadow hover:bg-slate-100'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='h-3 w-3'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
