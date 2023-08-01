88bfa6374f341c2e2ccd60c5bbe52f8713c7ee87
Ha Yen <hanguyentieuyen@gmail.com>
2023-08-02 00:06:51 +0700

    update product ui

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Product() {
   return (
     <Link to='/'>
-      <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.0625rem] hover:shadow-md'>
+      <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.05rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%] '>
           <img
             src='https://down-vn.img.susercontent.com/file/142753bff0653a5c452cb0e0b9d68731_tn'
@@ -12,7 +12,7 @@ export default function Product() {
           />
         </div>
         <div className='overflow-hidden p-2'>
-          <div className='min-h-[1.75rem] text-sm line-clamp-2'>
+          <div className='min-h-[2rem] text-xs line-clamp-2'>
             Chuột không dây Logitech B170 - USB, nhỏ gọn, thuận cả 2 tay, phù hợp PC/Laptop
           </div>
           <div className='mt-3 flex items-center'>
