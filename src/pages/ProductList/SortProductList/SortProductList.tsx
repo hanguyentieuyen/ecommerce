cf1a87168ddc70865ba1df329ba3b4f19ac2a638
Ha Yen <hanguyentieuyen@gmail.com>
2023-08-03 23:45:41 +0700

    product types

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -13,7 +13,10 @@ export default function SortProductList() {
           <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
             Bán chạy
           </button>
-          <select className='h-8 bg-white px-4 text-left text-sm capitalize text-black outline-none hover:bg-slate-100'>
+          <select
+            defaultValue=''
+            className='h-8 bg-white px-4 text-left text-sm capitalize text-black outline-none hover:bg-slate-100'
+          >
             <option value='' disabled>
               Giá
             </option>
