b28254e2f78e8eceb23345605414ff2531bbd12f
Ha Yen <43478102+hanguyentieuyen@users.noreply.github.com>
2023-08-06 15:45:02 +0000

    render product list, format number

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,28 +1,30 @@
 import { Link } from 'react-router-dom'
-
-export default function Product() {
+import { Product as ProductType } from 'src/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
+interface Props {
+  product: ProductType
+}
+export default function Product({ product }: Props) {
   return (
     <Link to='/'>
       <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.05rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%] '>
           <img
-            src='https://down-vn.img.susercontent.com/file/142753bff0653a5c452cb0e0b9d68731_tn'
-            alt=''
+            src={product.image}
+            alt={product.name}
             className='absolute left-0 top-0 h-full w-full bg-white object-cover'
           />
         </div>
         <div className='overflow-hidden p-2'>
-          <div className='min-h-[2rem] text-xs line-clamp-2'>
-            Chuột không dây Logitech B170 - USB, nhỏ gọn, thuận cả 2 tay, phù hợp PC/Laptop
-          </div>
+          <div className='min-h-[2rem] text-xs line-clamp-2'>{product.name}</div>
           <div className='mt-3 flex items-center'>
             <div className='max-w-[50%] truncate text-gray-500 line-through'>
               <span>₫</span>
-              <span>249.00</span>
+              <span>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className='ml-1 truncate text-orange'>
               <span>₫</span>
-              <span>175.000</span>
+              <span>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
@@ -62,7 +64,7 @@ export default function Product() {
               </div>
             </div>
             <div className='ml-2 text-sm'>
-              <span>32.2k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
