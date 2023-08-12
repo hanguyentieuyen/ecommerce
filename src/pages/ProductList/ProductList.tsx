428840e34914be8afd2c3a9ea341c632ad432e32
Ha Yen <43478102+hanguyentieuyen@users.noreply.github.com>
2023-08-12 18:44:03 +0000

    sort product by view/sold/createdAt

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -29,7 +29,7 @@ export default function ProductList() {
   const { data } = useQuery({
     queryKey: ['products', queryConfig],
     queryFn: () => {
-      return productApi.getProducts(queryConfig as ProductListConfig)
+      return productApi.getProducts(queryConfig)
     },
     keepPreviousData: true
   })
@@ -43,7 +43,7 @@ export default function ProductList() {
               <AsideFilter />
             </div>
             <div className='col-span-9'>
-              <SortProductList />
+              <SortProductList queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
               <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                 {data.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -1,16 +1,58 @@
-export default function SortProductList() {
+import { sortBy } from 'src/constanst/product'
+import { QueryConfig } from '../ProductList'
+import { ProductListConfig } from 'src/types/product.type'
+import classNames from 'classnames'
+import { createSearchParams, useNavigate } from 'react-router-dom'
+import path from 'src/constanst/path'
+interface Props {
+  queryConfig: QueryConfig
+  pageSize: number
+}
+export default function SortProductList({ queryConfig, pageSize }: Props) {
+  const navigate = useNavigate()
+  const { sort_by = sortBy.view } = queryConfig
+  const isActive = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    return sortByValue === sort_by
+  }
+  const handleSort = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    return navigate({
+      pathname: path.home,
+      search: createSearchParams({
+        ...queryConfig,
+        sort_by: sortByValue.toString()
+      }).toString()
+    })
+  }
   return (
     <div className='bg-gray-300/40 px-3 py-4'>
       <div className='flex flex-wrap items-center justify-between gap-2'>
         <div className='flex flex-wrap items-center gap-2'>
           <div>Sắp xếp theo</div>
-          <button className='h-8 bg-orange px-4 text-center text-sm capitalize text-white hover:bg-orange/80'>
+          <button
+            className={classNames('h-8 px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActive(sortBy.view),
+              'bg-white text-black hover:bg-slate-100': !isActive(sortBy.view)
+            })}
+            onClick={() => handleSort(sortBy.view)}
+          >
             Phổ biến
           </button>
-          <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
+          <button
+            className={classNames('h-8 px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActive(sortBy.createdAt),
+              'bg-white text-black hover:bg-slate-100': !isActive(sortBy.createdAt)
+            })}
+            onClick={() => handleSort(sortBy.createdAt)}
+          >
             Mới nhất
           </button>
-          <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
+          <button
+            className={classNames('h-8 px-4 text-center text-sm capitalize ', {
+              'bg-orange text-white hover:bg-orange/80': isActive(sortBy.sold),
+              'bg-white text-black hover:bg-slate-100': !isActive(sortBy.sold)
+            })}
+            onClick={() => handleSort(sortBy.sold)}
+          >
             Bán chạy
           </button>
           <select
