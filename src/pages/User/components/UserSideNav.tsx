4d43321817247a3e5323a23a055ed3917d004702
Ha Yen <hanguyentieuyen@gmail.com>
2023-09-22 01:12:42 +0700

    user sidenav layout

--- src/pages/User/components/UserSideNav.tsx
+++ src/pages/User/components/UserSideNav.tsx
@@ -1,3 +1,69 @@
+import { Link } from 'react-router-dom'
+import path from 'src/constant/path'
+
 export default function UserSideNav() {
-  return <div>UserSideNav</div>
+  return (
+    <div>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link to={path.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-black/10'>
+          <img
+            src='https://avatars.githubusercontent.com/u/43478102?v=4'
+            alt='avatar'
+            className='w-full rounded-full object-cover'
+          />
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate font-semibold text-gray-600'>Hayen</div>
+          <Link to={path.profile} className='flex items-center capitalize'>
+            <svg
+              width={12}
+              height={12}
+              viewBox='0 0 12 12'
+              xmlns='http://www.w3.org/2000/svg'
+              style={{ marginRight: 4 }}
+            >
+              <path
+                d='M8.54 0L6.987 1.56l3.46 3.48L12 3.48M0 8.52l.073 3.428L3.46 12l6.21-6.18-3.46-3.48'
+                fill='#9B9B9B'
+                fillRule='evenodd'
+              />
+            </svg>
+            Sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-7'>
+        <Link to={path.profile} className='flex items-center capitalize text-orange transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <img
+              className='h-full w-full'
+              alt=''
+              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
+            />
+          </div>
+          Tài khoản của tôi
+        </Link>
+        <Link to={path.profile} className='mt-4 flex items-center capitalize text-orange transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <img
+              className='h-full w-full'
+              alt=''
+              src='https://down-vn.img.susercontent.com/file/ba61750a46794d8847c3f463c5e71cc4'
+            />
+          </div>
+          Đổi mật khẩu
+        </Link>
+        <Link to={path.profile} className='mt-4 flex items-center capitalize text-orange transition-colors'>
+          <div className='mr-3 h-[22px] w-[22px]'>
+            <img
+              alt=''
+              className='h-full w-full'
+              src='https://down-vn.img.susercontent.com/file/f0049e9df4e536bc3e7f140d071e9078'
+            />
+          </div>
+          Đơn mua
+        </Link>
+      </div>
+    </div>
+  )
 }
