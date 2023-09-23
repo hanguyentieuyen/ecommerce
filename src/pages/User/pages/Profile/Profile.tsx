39af37454d4c074c3629b2d642d02cda68b58b8e
Ha Yen <hanguyentieuyen@gmail.com>
2023-09-23 12:41:44 +0700

    profile ui

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,3 +1,40 @@
+import Input from 'src/components/Input'
+
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='rounded-sm bg-white px-7 pb-20 shadow'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-600'>Hồ sơ của tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mât tài khoản</div>
+      </div>
+      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <form className='mt-6 flex-grow pr-12 md:mt-0'>
+          <div className='flex flex-wrap'>
+            <div className='w-[20%] pt-3 text-right capitalize'>Email</div>
+            <div className='w-[80%] pl-5'>
+              <div className='pt-3 text-gray-700'>hanguyen*******@gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-6 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Tên</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Số điện thoại</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap'>
+            <div className='w-[20%] truncate pt-3 text-right capitalize'>Địa chỉ</div>
+            <div className='w-[80%] pl-5'>
+              <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
