85bf91ed4e92810d412033943a5f9d2673ee06ca
Ha Yen <hanguyentieuyen@gmail.com>
2023-09-24 15:39:04 +0700

    update profile ui

--- src/pages/User/layouts/UserLayout/UserLayout.tsx
+++ src/pages/User/layouts/UserLayout/UserLayout.tsx
@@ -6,7 +6,7 @@ export default function UserLayout() {
     <div className='bg-neutral-100 py-16 text-sm text-gray-600'>
       <div className='container'>
         <div className='grid grid-cols-1 gap-6 md:grid-cols-12'>
-          <div className='lg-col-span-2 md:col-span-3'>
+          <div className='md:col-span-3 lg:col-span-2'>
             <UserSideNav />
           </div>
           <div className='md:col-span-9 lg:col-span-10'>
--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -2,38 +2,72 @@ import Input from 'src/components/Input'
 
 export default function Profile() {
   return (
-    <div className='rounded-sm bg-white px-7 pb-20 shadow'>
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
       <div className='border-b border-b-gray-200 py-6'>
         <h1 className='text-lg font-medium capitalize text-gray-600'>Hồ sơ của tôi</h1>
         <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mât tài khoản</div>
       </div>
       <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
         <form className='mt-6 flex-grow pr-12 md:mt-0'>
-          <div className='flex flex-wrap'>
-            <div className='w-[20%] pt-3 text-right capitalize'>Email</div>
-            <div className='w-[80%] pl-5'>
+          <div className='flex flex-col flex-wrap sm:flex-row'>
+            <div className='pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+            <div className='pl-5 sm:w-[80%]'>
               <div className='pt-3 text-gray-700'>hanguyen*******@gmail.com</div>
             </div>
           </div>
-          <div className='mt-6 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Tên</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Tên</div>
+            <div className='pl-5 sm:w-[80%]'>
               <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
             </div>
           </div>
-          <div className='mt-2 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Số điện thoại</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Số điện thoại</div>
+            <div className='pl-5 sm:w-[80%]'>
               <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
             </div>
           </div>
-          <div className='mt-2 flex flex-wrap'>
-            <div className='w-[20%] truncate pt-3 text-right capitalize'>Địa chỉ</div>
-            <div className='w-[80%] pl-5'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Địa chỉ</div>
+            <div className='pl-5 sm:w-[80%]'>
               <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Ngày sinh</div>
+            <div className='pl-5 sm:w-[80%]'>
+              <div className='flex justify-between'>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Ngày</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Tháng</option>
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Năm</option>
+                </select>
+              </div>
             </div>
           </div>
         </form>
+        <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                className='w-full rounded-full object-cover'
+                src='https://avatars.githubusercontent.com/u/43478102?v=4'
+                alt='avarta'
+              />
+            </div>
+            <input type='file' className='hidden' accept='.png, .jpg, .jpeg' />
+            <button className='text-gray-60 flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-gray-400'>
+              <div>Dung lượng file tối đa 1 MB</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
