f7430f13439b4879b2ff3148071d570d37865eea
Ha Yen <hanguyentieuyen@gmail.com>
2023-10-01 15:28:04 +0700

    yup validate change password

--- src/pages/User/pages/ChangePassword/ChangePassword.tsx
+++ src/pages/User/pages/ChangePassword/ChangePassword.tsx
@@ -1,3 +1,114 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import userApi from 'src/apis/user.api'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
+import { ErrorResponseApi } from 'src/types/utils.type'
+import { UserSchema, userSchema } from 'src/utils/rule'
+import { isAxiosUnprocessableEntityError } from 'src/utils/utils'
+
+type FormData = Pick<UserSchema, 'password' | 'new_password' | 'confirm_password'>
+const passwordSchema = userSchema.pick(['password', 'new_password', 'confirm_password'])
+
 export default function ChangePassword() {
-  return <div>ChangePassword</div>
+  const updateProfileMutation = useMutation(userApi.updateProfile)
+
+  const methods = useForm<FormData>({
+    defaultValues: {
+      password: '',
+      new_password: '',
+      confirm_password: ''
+    },
+    resolver: yupResolver<FormData>(passwordSchema)
+  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    reset
+  } = methods
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const res = await updateProfileMutation.mutateAsync(omit(data, ['confirm_password']))
+      toast.success(res.data.message)
+      reset()
+    } catch (error) {
+      if (isAxiosUnprocessableEntityError<ErrorResponseApi<FormData>>(error)) {
+        const formError = error.response?.data.data
+        if (formError) {
+          Object.keys(formError).forEach((key) => {
+            setError(key as keyof FormData, {
+              message: formError[key as keyof FormData],
+              type: 'Server'
+            })
+          })
+        }
+      }
+    }
+  })
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-600'>Hồ sơ của tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mât tài khoản</div>
+      </div>
+      <form onSubmit={onSubmit} className='max-width-2xl mr-auto mt-8'>
+        <div className='mt-6 flex-grow pr-12 md:mt-0'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Mật khẩu cũ</div>
+            <div className='pl-5 sm:w-[80%]'>
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                register={register}
+                name='password'
+                type='password'
+                placehoder='Mật khẩu cũ'
+                errorMessage={errors.password?.message}
+              />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Mật khẩu mới</div>
+            <div className='pl-5 sm:w-[80%]'>
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                register={register}
+                name='new_password'
+                placehoder='Mật khẩu mới'
+                errorMessage={errors.new_password?.message}
+              />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Xác nhận mật khẩu</div>
+            <div className='pl-5 sm:w-[80%]'>
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                register={register}
+                name='confirm_password'
+                placehoder='Xác nhận mật khẩu'
+                errorMessage={errors.confirm_password?.message}
+              />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'></div>
+            <div className='pl-5 sm:w-[80%]'>
+              <Button
+                type='submit'
+                className='flex h-9 items-center rounded-sm bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
+              >
+                Lưu
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
