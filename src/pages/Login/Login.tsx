d2987998f0d1bca03b078aa781efbcaf8b9d1154
Ha Yen <hanguyentieuyen@gmail.com>
2023-10-09 23:52:06 +0700

    seo for react spa, install react-helmet

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useMutation } from '@tanstack/react-query'
 import { useContext } from 'react'
+import { Helmet } from 'react-helmet'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import authApi from 'src/apis/auth.api'
@@ -51,6 +52,10 @@ export default function Login() {
   })
   return (
     <div className='bg-orange'>
+      <Helmet>
+        <title>Login | Shoppe clone</title>
+        <meta name='description' content='Login in Shoppee page' />
+      </Helmet>
       <div className='container'>
         <div className='grid-cols grid py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
