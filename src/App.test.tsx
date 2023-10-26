3c55213caea21c231c913719cab3f13fe0174a67
Ha Yen <hanguyentieuyen@gmail.com>
2023-10-27 01:59:31 +0700

    install testing lib router

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, test } from 'vitest'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import matchers from '@testing-library/jest-dom/matchers'
+
+expect.extend(matchers)
+
+
